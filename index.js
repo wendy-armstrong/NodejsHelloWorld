268f7e8d47f89d0c6826bf493275aefa74e04694
Wendy Armstrong <48316417+wendy-armstrong@users.noreply.github.com>
2019-07-16 13:46:23 +0100

    extra lines in output

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs-extra');
-let content = ' Hello world ' +  new Date() ;
+let content = ' Hello world ' +  new Date() + '\n this is line 2\n this is line 3';
+
 console.log(content);
 fs.writeFile('output.txt', content);
